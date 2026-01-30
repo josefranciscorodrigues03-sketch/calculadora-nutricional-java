f447f0583ffea8f20bb430dd438beba1bf00569e
José Francisco <josefranciscorodrigues03@gmail.com>
2026-01-29 23:28:01 -0300

    Refactor Tmb method to include choice parameter

--- Exercicios/src/CalculadoraNutricional/Calculos.java
+++ Exercicios/src/CalculadoraNutricional/Calculos.java
@@ -8,16 +8,31 @@ public class Calculos {
         var agua = peso*35;
         return agua;
     }
-    public static double Tmb(double peso, double idade, double altura, double genero) {
+    public static double Tmb(double peso, int idade, int altura, double genero, int escolha) {
+        double taxa;
         if (genero == 1) {
-            var taxa = 66.5 + (13.75*peso) + (5.003 * altura) - (6.75*idade);
-            return taxa;
+            taxa = 66.5 + (13.75*peso) + (5.003 * altura) - (6.75*idade);
         } else if (genero == 2) {
-            var taxa = 655.1 + (9.563*peso) + (1.850 * altura) - (4.676 * idade);
-            return taxa;
+            taxa = 655.1 + (9.563*peso) + (1.850 * altura) - (4.676 * idade);
         } else {
             System.out.println("você não escolheu");
             return 0;
         }
+        switch (escolha) {
+            case 1:
+                return taxa*1.2;
+            case 2:
+                return taxa*1.37;
+            case 3:
+                return taxa*1.55;
+            case 4:
+                return taxa*1.72;
+            case 5:
+                return taxa*1.9;
+            default:
+                System.out.println("Invalido");
+                return 0;
+        }
     }
 }
+
